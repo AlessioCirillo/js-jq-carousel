27c44adb9e5e759af8c8d8ce68ce80c03908158a
alessio <alessio.cirillo.1995@gmail.com>
2020-10-27 15:16:27 +0100

    js functional

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+$(document).ready( function (){
+
+    var next = $('.next');
+    var prev = $('.prev');
+
+    //IMPOSTAZIONI AL CLICK
+    next.click( function(){
+        nextPrevClick(next);
+    })
+
+    prev.click( function(){
+        nextPrevClick(prev);
+    })
+
+    //IMPOSTAZIONE FUNZIONE
+    function nextPrevClick(direzione){
+
+        var activeImg = $('.images img.active');
+        var activeCircle = $('.nav i.active');
+
+        //RIMOZIONE ACTIVE
+        activeImg.removeClass('active');
+        activeCircle.removeClass('active');
+
+        //DIREZIONE NEXT
+        if ( direzione === next ){
+            if( activeImg.hasClass('last') === true ){
+                $('.images img.first').addClass('active'); 
+                $('.nav i.first').addClass('active'); 
+            } else {
+                activeImg.next('img').addClass('active');
+                activeCircle.next('i').addClass('active');
+            }
+        }
+
+        //DIREZIONE PREV
+        if ( direzione === prev ){
+            if( activeImg.hasClass('first') === true ){
+                $('.images img.last').addClass('active'); 
+                $('.nav i.last').addClass('active'); 
+            } else {
+                activeImg.prev('img').addClass('active');
+                activeCircle.prev('i').addClass('active');
+            }
+        }
+    }
+
+});//--- end document
